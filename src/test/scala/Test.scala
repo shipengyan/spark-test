a62ab3fc575075eaf6ce827b70ccb1f193b80bf4
shi.pengyan <spy19881221@163.com>
2016-11-20 19:55:35 +0800

    first commit

---
+++ src/test/scala/Test.scala
@@ -0,0 +1,11 @@
+/**
+  * Created by spy on 2016-11-19.
+  */
+object Test {
+
+  def main(args: Array[String]) {
+    println("Hello World!")
+    //    val sparkConf = new SparkConf().setMaster("local").setAppName("test")
+    //    val sparkContext = new SparkContext(sparkConf)
+  }
+}
